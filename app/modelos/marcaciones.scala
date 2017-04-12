c20bd69e0b5a6b77fe92b7558c872cee8ad71815
irisariana <iris.ariana07@gmail.com>
2017-04-12 17:41:22 -0400

    Primera versión de WS con Slick

--- app/modelos/marcaciones.scala
+++ app/modelos/marcaciones.scala
@@ -3,28 +3,16 @@ import scala.util.Try
 import scala.util.Success
 import scala.util.Failure
 import java.util.Date
-import java.util.TimeZone
+import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.util._
+import org.joda.time.DateTime // importando la clase DateTime de JodA.
 
-import org.joda.time.DateTime
-import org.joda.time.Interval
-import org.joda.time.Period
-import org.joda.time.format.PeriodFormat
-import org.joda.time.LocalDateTime
-import org.joda.time.DateTimeZone
 
-case class Usuario(
-  id: Long,
-  email: String,
-  apellido: String,
-  nombre: String,
-  password: String
-){
-  def nombre_completo = {
-    val Nombre = nombre.toUpperCase()
-    s" $Nombre ${apellido.toUpperCase()}"
-  }
-}
+
+case class DatosUsuario(
+  correo: String    
+)
 
 case class Marcacion(
   id: Long,
@@ -33,28 +21,130 @@ case class Marcacion(
   lugar: String
 )
 
-case class DatosMarcacion(
+case class DatosCrearUsuario(
   email: String,
-  nombre_apellido: String,
-  fecha: String,
-  lugar: String
+  apellido: String,
+  nombre: String,
+  password: String    
 )
 
 object Marcaciones{
+	
   var usuarios: Seq[Usuario] = Seq[Usuario](
     Usuario(1,"pabloislas@gmail.com","Islas","Pablo","pislas123"),
     Usuario(2,"williambeowulf@gmail.com","Caceres","William","william123")
   )
+  var marcaciones: Seq[Marcacion] = Seq[Marcacion]()
   
-  val format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
+
+  def findUsuarios(email: Option[String],nombre: Option[String]) = {
+    val u1 = email.map { e =>
+      usuarios.filter { _.email == e }
+    }.getOrElse(usuarios)
+    val u2 = nombre.map{ n =>
+      u1.filter{_.nombre == n}
+    }.getOrElse(u1)
+    u2
+  }
+
+   /*Formato de fecha prevista : "dd-MM-yyyy hh:mm:ss"
+   @joaquin.olivera
+  */
+    // Lista de lugares donde se realizan las marcaciones...
+ // var lugares : Seq[String] = Seq()
+    // Calculo de variables medidas de tiempo(para no tener valores magicos...)
+  val secondsInMilli: Long = 1000
+  val minutesInMilli: Long = secondsInMilli * 60
+	val hoursInMilli: Long = minutesInMilli * 60
+	val daysInMilli: Long = hoursInMilli * 24
+	
+  def toDate(fecha:String): Date = {
+	  val formatOfDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	  formatOfDate.parse(fecha)
+  }
   
-  var marcaciones: Seq[Marcacion] = Seq[Marcacion](  
-    Marcacion(1, 1, format.parse("31-03-2017 08:00:39"), "ES"),
-    Marcacion(2, 1, format.parse("31-03-2017 18:00:39"), "ES"),
-    Marcacion(3, 2, format.parse("31-03-2017 09:00:39"), "ES"),
-    Marcacion(4, 2, format.parse("31-03-2017 19:00:39"), "ES"),
-    Marcacion(5, 2, format.parse("28-03-2017 09:30:39"), "ES")
-  )
+  def _createMarking(usuario_id: Long,lugar: String,fecha: Date): Try[Marcacion] = {
+    val m1 = Marcacion(marcaciones.length+1,usuario_id,fecha,lugar)
+    marcaciones = marcaciones.:+(m1)
+    /*Failure(new Exception("Error al insertertar en la base de datos"))*/Success(m1)
+  }
+  
+  def _newMarking(email:String,lugar:String, fecha:String): Try[Marcacion] = {
+    for {
+     user <- findUsuario(email)
+     mark <- _createMarking(user.id, lugar, toDate(fecha))
+    }yield(mark)
+  }
+  
+  def listingMarks(email:String, fecha: String): Seq[Marcacion] = {
+    val fechaToDate = toDate(fecha)
+     if(findUsuario(email).toOption.isDefined){
+        marcaciones.filter(marcacion => 
+       ((marcacion.usuario_id == (findUsuario(email).toOption.get).id) && 
+       ((marcacion.fecha.getTime - fechaToDate.getTime) / daysInMilli == 0)))
+     }else{
+       println("No existe el Usuario solicitado\n")
+       Seq[Marcacion]()
+     }
+  }
+  
+  def listingDate(fecha: String): Seq[Marcacion] = {
+		// todas las marcaciones de la fecha...
+		marcaciones.filter(marca => ((marca.fecha.getTime - toDate(fecha).getTime)/ daysInMilli) == 0)
+  }
+  
+   // las iteraciones se puede hacer con recursividad y el trabajo con elementos de una lista, mediante .head y .tail.head
+  
+ /* def calculateWork(email:String, fecha:String, listaDeMarcaciones: Seq[Marcacion], workedTime): Try[Long] = {
+    if (listaDeMarcaciones.isEmpty){
+      
+    }else {
+      workedTime += math.abs(listaDeMarcaciones.head.fecha.getTime() - listaDeMarcaciones.tail.head.fecha.getTime())
+      calculateWork(email,fecha,listaDeMarcaciones.tail.tail,workedTime)
+    }
+      println(s"Trabajado: ${workedTime / hoursInMilli} H ${(workedTime / minutesInMilli) - (workedTime / hoursInMilli)*60}M")
+      Success(workedTime.toLong)
+  }*/
+  
+  def _calculateWorkTimeEven (email: String, fecha:String): Try[Long] = {
+      var i = listingMarks(email, fecha).iterator  // 
+      var workedTime: Long = 0
+      do{
+        workedTime += math.abs(i.next.fecha.getTime() - i.next.fecha.getTime()) // como usar 2 elementos de la lista en una funcion i.e map?
+      }while(i.hasNext != false)
+       println(s"Trabajado: ${workedTime / hoursInMilli} H ${(workedTime / minutesInMilli) - (workedTime / hoursInMilli)*60}M")
+      Success(workedTime)
+  }
+ 
+  def _calculateWorkTimeNotEven (email: String, fecha:String) :Try[Long] = {
+      var i = listingMarks(email, fecha).init.iterator // se olvida del ultimo porque no pertenece a una sesion cerrada...
+      var workedTime: Long = 0
+      do{ // hacer las iteraciones con .map
+        workedTime += math.abs(i.next.fecha.getTime() - i.next.fecha.getTime()) // DAte 
+      }while(i.hasNext != false)
+       println(s"Trabajado: ${workedTime / hoursInMilli} H ${(workedTime / minutesInMilli) - (workedTime / hoursInMilli)*60} M")
+      Success(workedTime)
+  }
+  
+  def timeWorked(email: String, fecha:String) : Try[Long] = {
+    if(listingMarks(email, fecha).size < 2){
+      Failure(new Exception(s"No hay registrada una sesion del usuario con email: $email"))
+    }else if(listingMarks(email, fecha).size % 2 == 0){
+      // si es un numero par de entradas se calculan el tiempo de las sesiones...
+      // calculateWork(email, fecha, listingMarks(email, fecha), 0)
+      _calculateWorkTimeEven(email, fecha)
+    }else{ // existe ademas una sesion abierta...
+      println(s"Todavia tiene sesion abierta desde las ${new SimpleDateFormat("HH:mm:ss").format(listingMarks(email, fecha).last.fecha)}")
+      //calculateWork(email, fecha, listingMarks(email, fecha).init, 0)
+      _calculateWorkTimeNotEven(email, fecha)
+    }
+  }
+  
+  /*
+   * Fin @joaquin.olivera...
+   * */
+
+  
   
   def findUsuario(email: String): Try[Usuario] = {
     usuarios.filter{usuario => usuario.email == email}.
@@ -62,8 +152,8 @@ object Marcaciones{
       Success(u)
     }.getOrElse(Failure(new Exception(s"No existe el usuario con email: $email")))
   }
-  
-  def validarEmailRepetido(email: String): Try[String] = {
+ 
+  def validarUsuario(email: String): Try[String] = {
     findUsuario(email).map{u =>
       Failure(new Exception(s"Ya existe el usuario con el email"))
     }.getOrElse(
@@ -79,9 +169,8 @@ object Marcaciones{
       usuarios = usuarios.:+(u)
       Some(u)  
     }*/
-    
     for{
-      _ <- validarEmailRepetido(email)
+      _ <- validarUsuario(email)
       u <- _crearUsuarioEnBd(email, apellido, nombre, password)
     }yield(u)
   }
@@ -94,101 +183,9 @@ object Marcaciones{
   /**
    * dd-m-yyyy hh:mm:ss
    */
-  
   def _agregarMarcacionExterna(email: String) = ???
-  
-  /* ============================ EJERCICIO 1 ============================== 
-   * 1.  Agregar marcacion para usuario [los campos se utilizan los que ya estan, se supone que los usuarios ya existen]. 
-   * Parametros que recibe:
-   1. email
-   2. fecha: String [dd-MM-yyyy  hh:mm:ss]
-       1. Ejemplo: 29-03-2017 16:32:00 */
-  
-  def agregarMarcacion(email: String, fechaHora: String, lugar: String):Try[Marcacion] = {
-              
-    for{
-      u <- findUsuario(email)
-      m <- (_crearMarcacionBD(u.id, fechaHora, lugar)
-      )
-    }yield(m)  
-  }  
-  
-  def _crearMarcacionBD(id: Long, fechaHora: String, lugar: String):Try[Marcacion] = {
-              
-    //val format = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
-       
-    val m1 = Marcacion(marcaciones.length+1, id, format.parse(fechaHora), lugar)
-    marcaciones = marcaciones.:+(m1)
-    Success(m1)
-  }  
-  
-    /* ============================ EJERCICIO 2 ============================== */
-  /* 2. Listar marcaciones por usuario ordenados por fecha. Parametros que recibe:
-   1. email
-   2. Option[fecha]*/
-  
-  
-   def listarMarcacionesUsuario(email: String): Try[Seq[DatosMarcacion]] = {
-    for{
-      u <- findUsuario(email)
-      n <- (
-        Success(marcaciones.filter{_.usuario_id == u.id}.
-                            sortBy(_.fecha).
-                            map(_datosMarcacion(u, _))
-               )
-      )
-    }yield(n)  
-  }
-    
-   def _datosMarcacion(u: Usuario, m: Marcacion): DatosMarcacion = {
-     DatosMarcacion(
-       u.email,
-       u.nombre_completo,
-       format.format(m.fecha),
-       m.lugar
-     )
-   }
-   
-    /* ============================ EJERCICIO 3 ============================== */
-  /* 3. Listar marcaciones por día ordenados por hora. Parametros que recibe:
-   1. fecha
-   2. retorno:
-       1. Persona - Fecha - Hora - Lugar*/
-  
-  
-   def listarMarcaciones(fechaElegida: String): Seq[Marcacion] = {       
-     val dtf = new SimpleDateFormat("dd-MM-yyyy")
-     marcaciones.filter{m => dtf.format(m.fecha) == fechaElegida}.sortBy(_.fecha)
-  }
-   
-    /* ============================ EJERCICIO 4 ============================== */
-   /* Obtener la diferencia entre dos fechas*/
-   
-   def difDate(entrada: String, salida: String) = {
-     
-     val dtf = new SimpleDateFormat("HH:mm:ss")
-     dtf.setTimeZone(TimeZone.getTimeZone("UTC"))
-     
-     val interval = new Interval(format.parse(entrada).getTime, format.parse(salida).getTime())
-	   val period = interval.toPeriod()
-     val time = dtf.format(interval.toDurationMillis())
-     
-	   println(s"La diferencia es: ${period.getYears()} anhos, ${period.getMonths()} meses, ${period.getDays()} dias, ${period.getHours()} horas, ${period.getMinutes()} minutos, ${period.getSeconds()} segundos") 
-	   println(s"La diferencia en formato HH:mm:ss es: $time")
-	     
-	   
-   }
-   
-   def difHora(entrada: String, salida: String) = {
-     
-     val dtf = new SimpleDateFormat("HH:mm:ss")
-     dtf.setTimeZone(TimeZone.getTimeZone("UTC"))
-     
-     val interval = new Interval(dtf.parse(entrada).getTime, dtf.parse(salida).getTime())
-     val time = dtf.format(interval.toDurationMillis())
-      
-	   println(s"La diferencia es: $time")
-	   
-   }
+
 }
 
+ 
+  
